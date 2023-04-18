0274f3c066060c674be1793a7521cc26b89afd03
mozizhao <382843430@qq.com>
2023-04-18 14:52:35 +0800

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ import random
 import signal
 import subprocess
 import threading
-
 import flask
 import psutil
 from flask import Flask, request, jsonify
@@ -38,7 +37,7 @@ def available_port():
         p = random.choice(list(range(available_ports_low_bound, available_ports_upper_bound)))
 
         try:
-            subprocess.check_output(f"lsof -i:{p}", shell=True).decode()
+            subprocess.check_output(["lsof", f"-i:{p}"], shell=False).decode()
         except Exception as e:
             empty = True
 
@@ -86,7 +85,7 @@ def collect_progress(jid):
 #     subprocess.call(f"rm -rf ./{jid}/progress.dat", shell=True)
 
 
-def start_job(jid, job_type, batch_size, workers):
+def start_job(jid, job_type, batch_size, workers, init_progress):
     logging.info(f"receive start_job request with data {request.json}")
     worker_str = ""
     for w in workers:
@@ -100,8 +99,8 @@ def start_job(jid, job_type, batch_size, workers):
         **os.environ,
         "CUDA_VISIBLE_DEVICES": f"{worker_str}",
     }
-    p = subprocess.Popen(["python3", "-m", "torch.distributed.launch", "--nproc_per_node", f"{len(workers)}", "--master_port", f"{port}", "ddp.py", f"{job_type}", f"{batch_size}", f"{jid}"], env=env, shell=False)
-    logging.info(f"executed command: 'CUDA_VISIBLE_DEVICES={worker_str} python3 -m torch.distributed.launch --nproc_per_node {len(workers)} --master_port {port} ddp.py {job_type} {batch_size} {jid}'. Create training process [{p.pid}] for {jid}")
+    p = subprocess.Popen(["python3", "-m", "torch.distributed.launch", "--nproc_per_node", f"{len(workers)}", "--master_port", f"{port}", "ddp.py", f"{job_type}", f"{batch_size}", f"{jid}", f"{init_progress}"], env=env, shell=False)
+    logging.info(f"executed command: 'CUDA_VISIBLE_DEVICES={worker_str} python3 -m torch.distributed.launch --nproc_per_node {len(workers)} --master_port {port} ddp.py {job_type} {batch_size} {jid} {init_progress}'. Create training process [{p.pid}] for {jid}")
 
     # jid_worker_association[jid] = []
     for w in workers:
@@ -135,9 +134,9 @@ def progress():
 
 @app.route('/start/', methods=['post'])
 def start():
-    jid, job_type, batch_size, workers = request.json['job_id'], request.json['job_type'], request.json['batch_size'], request.json['workers']
+    jid, job_type, batch_size, workers, init_progress = request.json['job_id'], request.json['job_type'], request.json['batch_size'], request.json['workers'], request.json['progress']
 
-    start_job(jid, job_type, batch_size, workers)
+    start_job(jid, job_type, batch_size, workers, init_progress)
     return 'success'
 
 
